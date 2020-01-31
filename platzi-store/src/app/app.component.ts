44e9a518e473aefd9be958ed349651a093227227
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-01-30 18:21:02 -0600

    Uso de ngFor y delete

--- platzi-store/src/app/app.component.ts
+++ platzi-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'perez'];
+  
+  addItem() {
+    this.items.push('nuevo item');
+  }
+}
+
+deleteItem(index: number){
+  this.items.splice(index, 1);
 }
