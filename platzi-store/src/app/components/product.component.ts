4c33b25bd5b1ed2b48ac565e2eb0865191fdfdff
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-05-22 19:49:07 -0500

    Estilospara mostrar la lista de productos

--- platzi-store/src/app/components/product.component.ts
+++ platzi-store/src/app/components/product.component.ts
@@ -14,7 +14,8 @@ import { Product } from '../product.model';
 
 @Component({
 selector: 'app-product',
-templateUrl: './product-component.html'
+templateUrl: './product.component.html',
+styleUrls: ['./product.component.scss']
 })
 
 export class  ProductComponent implements OnInit, DoCheck, OnDestroy{
@@ -23,7 +24,7 @@ export class  ProductComponent implements OnInit, DoCheck, OnDestroy{
 @Output() ProductClicked: EventEmitter<any> = new EventEmitter();
 
 constructor() {
-  console.log('constructor')
+  console.log('constructor');
 }
 
 // ngOnChanges(changes: SimpleChanges) {
@@ -35,6 +36,7 @@ ngOnInit() {
   console.log('onInit');
 }
 
+// no se recomienda usar ngDocheck() y ngOnChanges() al mismo tiempo.
 ngDoCheck() {
   console.log('doCheck');
 }
