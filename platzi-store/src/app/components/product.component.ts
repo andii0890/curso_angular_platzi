5a77ad8e5ef91c5a2f3667cee1c571c1caafd4f8
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-05-22 19:11:05 -0500

    Ciclo de vida de los componentes

--- platzi-store/src/app/components/product.component.ts
+++ platzi-store/src/app/components/product.component.ts
@@ -1,4 +1,15 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy,
+} from '@angular/core';
+
 import { Product } from '../product.model';
 
 @Component({
@@ -6,14 +17,35 @@ selector: 'app-product',
 templateUrl: './product-component.html'
 })
 
-export class  ProductComponent {
+export class  ProductComponent implements OnInit, DoCheck, OnDestroy{
 
-	@Input() product: Product;
-	@Output() ProductClicked: EventEmitter<any> = new EventEmitter();
+@Input() product: Product;
+@Output() ProductClicked: EventEmitter<any> = new EventEmitter();
 
-	addCart(){
-	console.log('anadir al carrito');
-	this.ProductClicked.emit(this.product.id);
+constructor() {
+  console.log('constructor')
+}
 
-	}
+// ngOnChanges(changes: SimpleChanges) {
+//   console.log('ngOnChanges');
+//   console.log(changes);
+// }
+
+ngOnInit() {
+  console.log('onInit');
+}
+
+ngDoCheck() {
+  console.log('doCheck');
+}
+
+ngOnDestroy() {
+  console.log('ngOnDestroy');
+}
+
+addCart() {
+console.log('anadir al carrito');
+this.ProductClicked.emit(this.product.id);
+
+  }
 }
