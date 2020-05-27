8c8fa8eb2cf84178a0c347b58d93fb652e446244
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-05-27 18:46:49 -0500

    Construyendo una directiva propia

--- platzi-store/src/app/exponential.pipe.ts
+++ platzi-store/src/app/exponential.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExponentialPipe implements PipeTransform {
 
-  transform(value: number){
+  transform(value: number) {
     return Math.pow(value, 2);
   }
 
