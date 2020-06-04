90429c2a7f29258bfb03117ba2b029976abe98ae
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-06-03 19:33:16 -0500

    Preparar el proyecto para lazy loading

---
+++ platzi-store/src/app/home/home-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, NgModuleRef } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    RouterModule
+  ]
+
+})
+
+export class HomeRoutingModule {}
