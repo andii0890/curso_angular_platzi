5a5721281be56d07331904263dc94349bd57afc7
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-09-17 14:53:29 -0500

    Ambientes en angular

--- platzi-store/src/app/demo/demo.module.ts
+++ platzi-store/src/app/demo/demo.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { DemoComponent } from './components/demo/demo.component';
 import { DemoRoutingModule } from './demo-routing.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     DemoComponent
   ], imports: [
     DemoRoutingModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 
